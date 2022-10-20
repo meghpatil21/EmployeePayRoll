a9bf92581fe1d898d0f80f11275091f986e83911
meghpatil21 <megh.after18@gmail.com>
2022-10-21 00:10:33 +0530

    Added monthly Wage

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,31 +7,41 @@
             
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
             Console.WriteLine();
+            int totalWage = 0;
 
-            int empWorkHours = 0, empWage = 0;
-            Random random = new Random();
-            int empCheck= random.Next(0,3);
-
-            switch(empCheck)
+            for (int i = 1; i < 21; i++)
             {
-                case 0: 
-                    Console.WriteLine("Employee is Present Full time");
-                    empWorkHours = empWorkHours + 8;
-                    empWage = empWorkHours * 20;
-                    break;
-                case 1:
-                    Console.WriteLine("Employee is Present Part time");
-                    empWorkHours= empWorkHours + 4;
-                    empWage= empWorkHours * 20;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    break ;
+                Console.WriteLine("Day "+i);
+
+                int empWorkHours = 0, empWage = 0; 
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Present Full time");
+                        empWorkHours = empWorkHours + 8;
+                        empWage = empWorkHours * 20;
+                        break;
+                    case 1:
+                        Console.WriteLine("Employee is Present Part time");
+                        empWorkHours = empWorkHours + 4;
+                        empWage = empWorkHours * 20;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                }
+                
+                totalWage=totalWage+empWage;
+
+                Console.WriteLine("Todays Employee Wage: " + empWage);
+                Console.WriteLine($"Total Wage till day {i} : "+totalWage);
+                Console.WriteLine();
+                //Already use Switch Statement so no changes
             }
 
-            Console.WriteLine("Todays Employee Wage: "+ empWage);
-
-            //Already use Switch Statement so no changes
         }
     }
 }
