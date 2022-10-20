2687d6400ccb79f5b686b854f9c3493379c91ca3
meghpatil21 <megh.after18@gmail.com>
2022-10-20 23:49:31 +0530

    Added Part time

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,14 +8,19 @@
 
             int empWorkHours = 0, empWage = 0;
             Random random = new Random();
-            int empCheck= random.Next(0,2);
+            int empCheck= random.Next(0,3);
 
             switch(empCheck)
             {
                 case 0: 
-                    Console.WriteLine("Employee is Present");
+                    Console.WriteLine("Employee is Present Full time");
                     empWorkHours = empWorkHours + 8;
                     empWage = empWorkHours * 20;
+                    break;
+                case 1:
+                    Console.WriteLine("Employee is Present Part time");
+                    empWorkHours= empWorkHours + 4;
+                    empWage= empWorkHours * 20;
                     break;
                 default:
                     Console.WriteLine("Employee is Absent");
