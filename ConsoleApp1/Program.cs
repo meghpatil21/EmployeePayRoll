3dd70829f5fa9860989b511089f667d6d0756337
meghpatil21 <megh.after18@gmail.com>
2022-10-20 23:31:48 +0530

    Added Employee Attendance

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
+
+            Random random = new Random();
+            int empCheck= random.Next(0,2);
+
+            switch(empCheck)
+            {
+                case 0: 
+                    Console.WriteLine("Employee is Present");
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break ;
+            }
+
+
         }
     }
 }
